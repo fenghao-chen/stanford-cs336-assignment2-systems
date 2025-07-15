cfa844a909de384b46e2185eb892a55288142df9
Fenghao Chen <huizhoucheng@gmail.com>
2025-07-15 12:57:22 -0700

    Fix issue - reshaping qkv into 2D tensors doesn't work for some reason. Now it's handled with batch size in mind

--- cs336_systems/flash_forward.py
+++ cs336_systems/flash_forward.py
@@ -5,57 +5,49 @@ import math
 class FlashForward(torch.autograd.Function):
     @staticmethod
     def forward(ctx, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, is_causal: bool = False) -> torch.Tensor:
-        batch_size = q.shape[0]
-        # Reshape them into 2D tensors
-        q = rearrange(q, "... d -> (...) d")
-        k = rearrange(k, "... d -> (...) d")
-        v = rearrange(v, "... d -> (...) d")
-
-        n_q, d_model = q.shape
-        n_k, _ = k.shape
+        batch_size, n_q, d_model = q.shape
+        _, n_k, _ = k.shape
         b_q = b_k = 16 # tile sizes
         t_q = math.ceil(n_q / b_q)
         t_k = math.ceil(n_k / b_k)
         O = torch.zeros_like(q)
-        L = torch.zeros(n_q)
+        L = torch.full((batch_size, n_q), float('-inf'))
 
         for i in range(t_q):
             q_start = i * b_q
             q_end = min(q_start + b_q, n_q)
-            q_i = q[q_start: q_end]
+            q_i = q[:, q_start: q_end, :]  # (batch_size, tile_size, d_model)
 
-            m = torch.empty(b_q).fill_(float("-inf")) # todo maybe incorrect ?
-            l = torch.zeros(b_q)
-            o = torch.zeros(b_q, d_model)
+            m = torch.full((batch_size, q_end - q_start), float('-inf'))
+            l = torch.zeros((batch_size, q_end - q_start))
+            o = torch.zeros_like(q_i)
 
             for j in range(t_k):
                 k_start = j * b_k
                 k_end = min(k_start + b_k, n_k)
 
-                k_j = k[k_start : k_end]
-                v_j = v[k_start : k_end]
+                k_j = k[:, k_start : k_end, :] # (batch_size, tile_size, d_model)
+                v_j = v[:, k_start : k_end, :] # (batch_size, tile_size, d_model)
 
-                attn_scores = einsum(q_i, k_j, 'queries d_k,  keys d_k -> queries keys') / math.sqrt(d_model)
-                m_new = torch.max(m, attn_scores.max(dim=1)[0])
+                attn_scores = einsum(q_i, k_j, '... queries d_k,  ... keys d_k -> ... queries keys') / math.sqrt(d_model)
+                m_new = torch.maximum(m, attn_scores.max(dim=-1)[0])
 
                 scale = torch.exp(m - m_new)
                 l = l * scale
-                o = o * scale[:, None]
+                o = o * scale.unsqueeze(-1)
 
-                p_tilde = torch.exp(attn_scores - m_new[:, None])
-                l += p_tilde.sum(dim=1)
+                p_tilde = torch.exp(attn_scores - m_new.unsqueeze(-1))
+                l += p_tilde.sum(dim=-1)
                 o += p_tilde @ v_j
 
                 m = m_new
 
-            o_i = o / l[:, None]
+            o_i = o / l.unsqueeze(-1)
             l_i = m + torch.log(l)
 
-            O[q_start : q_end] = o_i
-            L[q_start : q_end] = l_i
+            O[:, q_start : q_end, :] = o_i
+            L[:, q_start : q_end] = l_i
 
-        O = rearrange(O, "(batch_size queries) d -> batch_size queries d", batch_size=batch_size)
-        L = rearrange(L, "(batch_size queries) -> batch_size queries", batch_size=batch_size)
         ctx.save_for_backward(L)
         return O
 
